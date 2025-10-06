0893b289fe4c841640e52ca5643ed68d1d098740
Random-Scientist <37155686+Random-Scientist@users.noreply.github.com>
2025-10-06 01:25:20 -0700

    i love heuristics

--- ui/src/nyaa.rs
+++ ui/src/nyaa.rs
@@ -3,7 +3,7 @@ use std::num::NonZeroUsize;
 use nyaa::{AnimeKind, SearchQuery};
 use serde::{Deserialize, Serialize};
 
-use crate::show::Show;
+use crate::{NameKind, show::Show};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub(crate) struct Config {
@@ -33,22 +33,78 @@ impl Show {
         episode: u32,
         cat: AnimeKind,
     ) -> impl Iterator<Item = nyaa::SearchQuery> + 'static {
-        let season = if self.relations.prequel.is_none() {
-            "01"
-        } else {
-            log::warn!("season number for shows with prequels not yet supported");
-            ""
-        };
-        self.names.iter().map(move |name| SearchQuery {
-            query: format!("{} S{season}E{episode:02}", &name.1),
-            category: nyaa::MediaCategory::Anime(Some(cat)),
-            filter: nyaa::Filter::NoFilter,
-            max_page_idx: const { NonZeroUsize::new(5) },
-            sort: nyaa::Sort {
-                by: nyaa::SortBy::Seeders,
+        if self.relations.prequel.is_some() {
+            log::warn!(
+                "season number for anilist shows with prequels not yet supported through anilist"
+            );
+        }
+        let season = self
+            .names
+            .iter()
+            .find_map(|v| {
+                fn parse_season(s: &str) -> Option<u32> {
+                    if let Ok(v) = s.parse() {
+                        // is this sane??
+                        if v >= 15 { None } else { Some(v) }
+                    } else {
+                        Some(match s {
+                            "first" | "one" => 1,
+                            "second" | "two" => 2,
+                            "third" | "three" => 3,
+                            "fourth" | "four" => 4,
+                            "fifth" | "five" => 5,
+                            "sixth" | "six" => 6,
+                            "seventh" | "seven" => 7,
+                            "eighth" | "eight" => 8,
+                            "ninth" | "nine" => 9,
+                            "tenth" | "ten" => 10,
+                            _ => return None,
+                        })
+                    }
+                }
+                if !matches!(v.0, NameKind::English | NameKind::Romaji) {
+                    return None;
+                }
+                let mut next = false;
+                let s =
+                    v.1.split(' ')
+                        .last()
+                        .map(|v| parse_season(&v.to_lowercase()))
+                        .flatten();
+                if s.is_some() {
+                    return s;
+                }
+                for word in v.1.split(' ') {
+                    let lower = word.to_lowercase();
+                    if next {
+                        let s = parse_season(&lower);
+                        if s.is_some() {
+                            return s;
+                        }
+                    } else if &lower == "season" {
+                        next = true;
+                    } else {
+                        next = false;
+                    }
+                }
+                None
+            })
+            .unwrap_or(1);
+
+        self.names
+            .iter()
+            .map(move |name| SearchQuery {
+                query: format!("{} S{season:02}E{episode:02}", &name.1),
+                category: nyaa::MediaCategory::Anime(Some(cat)),
+                filter: nyaa::Filter::NoFilter,
+                max_page_idx: const { NonZeroUsize::new(5) },
+                sort: nyaa::Sort {
+                    by: nyaa::SortBy::Seeders,
+                    ..Default::default()
+                },
                 ..Default::default()
-            },
-            ..Default::default()
-        }).collect::<Vec<_>>().into_iter()
+            })
+            .collect::<Vec<_>>()
+            .into_iter()
     }
 }
